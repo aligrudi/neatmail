43db5b374e91c079fffb663263bf7c3250f5c314
Ali Gholami Rudi <ali@rudi.ir>
2016-05-23 21:26:20 +0430

    mk: read invalid utf-8 characters byte by byte

--- mk.c
+++ mk.c
@@ -10,18 +10,16 @@
 static int uc_len(char *s)
 {
 	int c = (unsigned char) s[0];
-	if (~c & 0x80)
+	if (~c & 0x80)		/* ASCII */
 		return c > 0;
+	if (~c & 0x40)		/* invalid UTF-8 */
+		return 1;
 	if (~c & 0x20)
 		return 2;
 	if (~c & 0x10)
 		return 3;
 	if (~c & 0x08)
 		return 4;
-	if (~c & 0x04)
-		return 5;
-	if (~c & 0x02)
-		return 6;
 	return 1;
 }
 
