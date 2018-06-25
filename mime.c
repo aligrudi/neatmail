3cb5e92019be316d299ad87520d9d13d5a5e02db
Ali Gholami Rudi <ali@rudi.ir>
2018-06-25 19:41:02 +0430

    mime: cp1256 and iso-8859-6 character sets

--- mime.c
+++ mime.c
@@ -76,6 +76,8 @@ static void dec_qp(struct sbuf *sb, char *s, char *e)
 	}
 }
 
+static void toutf8(char *charset, struct sbuf *dst, char *src, int len);
+
 static void msg_hdrdec2(struct sbuf *sb, char *hdr, char *end)
 {
 	while (hdr < end) {
@@ -85,13 +87,22 @@ static void msg_hdrdec2(struct sbuf *sb, char *hdr, char *end)
 		char *q4 = q3 ? memchr(q3 + 1, '?', end - q3) : NULL;
 		if (q1 && q2 && q3 && q4 && q4[1] == '=') {
 			int c = tolower((unsigned char) q2[1]);
+			struct sbuf *sb2 = sbuf_make();
+			char charset[128];
+			int charset_len = q2 - q1 - 1;
+			if (charset_len > sizeof(charset) - 1)
+				charset_len = sizeof(charset) - 1;
+			memcpy(charset, q1 + 1, charset_len);
+			charset[charset_len] = '\0';
 			if (c == 'b')
-				dec_b64(sb, q3 + 1, q4);
+				dec_b64(sb2, q3 + 1, q4);
 			else
-				dec_qp(sb, q3 + 1, q4);
+				dec_qp(sb2, q3 + 1, q4);
 			hdr = q4 + 2;
 			while (isspace((unsigned char) *hdr) && hdr + 1 < end)
 				hdr++;
+			toutf8(charset, sb, sbuf_buf(sb2), sbuf_len(sb2));
+			sbuf_free(sb2);
 		} else {
 			sbuf_chr(sb, (unsigned char) *hdr++);
 		}
@@ -158,6 +169,17 @@ static void read_boundary(struct mime *m, char *s, char *hdrend)
 	m->depth++;
 }
 
+static char *read_charset(struct mime *m, char *s, char *hdrend)
+{
+	if (!(s = memchr(s, '=', hdrend - s)))
+		return NULL;
+	if (s + 14 < hdrend && startswith(s, "=\"windows-1256\""))
+		return "windows-1256";
+	if (s + 12 < hdrend && startswith(s, "=\"iso-8859-6\""))
+		return "iso-8859-6";
+	return NULL;
+}
+
 static char *hdr_nextfield(char *s, char *e)
 {
 	while (s && s < e && *s != ';')
@@ -167,11 +189,12 @@ static char *hdr_nextfield(char *s, char *e)
 	return s && s + 2 < e ? s + 1 : NULL;
 }
 
-static int read_hdrs(struct mime *m, struct sbuf *dst)
+static void read_hdrs(struct mime *m, struct sbuf *dst, int *type, char **charset)
 {
 	char *s = m->src;
 	char *e = m->end;
-	int type = 0;
+	*type = 0;
+	*charset = NULL;
 	while (s && s < e && *s != '\n') {
 		char *n = memchr(s, '\n', e - s);
 		while (n && n + 1 < e && n[1] != '\n' && isspace(n[1]))
@@ -182,12 +205,14 @@ static int read_hdrs(struct mime *m, struct sbuf *dst)
 			char *key = strchr(s, ':') + 1;
 			char *hdrend = s + hdrlen(s, e - s);
 			while (key) {
-				while (key < hdrend && isspace(*key))
+				while (key < hdrend && isspace((unsigned char) *key))
 					key++;
 				if (startswith(key, "text"))
-					type |= TYPE_TXT;
+					*type |= TYPE_TXT;
 				if (startswith(key, "multipart"))
-					type |= TYPE_MPART;
+					*type |= TYPE_MPART;
+				if (startswith(key, "charset"))
+					*charset = read_charset(m, key, hdrend);
 				if (startswith(key, "boundary"))
 					read_boundary(m, key, hdrend);
 				key = hdr_nextfield(key, hdrend);
@@ -200,9 +225,9 @@ static int read_hdrs(struct mime *m, struct sbuf *dst)
 				while (key < hdrend && isspace(*key))
 					key++;
 				if (startswith(key, "quoted-printable"))
-					type |= ENC_QP;
+					*type |= ENC_QP;
 				if (startswith(key, "base64"))
-					type |= ENC_B64;
+					*type |= ENC_B64;
 				key = hdr_nextfield(key, hdrend);
 			}
 		}
@@ -211,7 +236,6 @@ static int read_hdrs(struct mime *m, struct sbuf *dst)
 	}
 	sbuf_chr(dst, '\n');
 	m->src = s + 1;
-	return type;
 }
 
 static int is_bound(struct mime *m, char *s)
@@ -273,8 +297,13 @@ int msg_demime(char *msg, long msglen, char **mod, long *modlen)
 	m.end = msg + msglen;
 	m.depth = 0;
 	while ((m.depth && m.src < m.end) || m.src == msg) {
-		int type = read_hdrs(&m, dst);
-		read_body(&m, dst, type);
+		int type;
+		char *charset;
+		struct sbuf *sb = sbuf_make();
+		read_hdrs(&m, sb, &type, &charset);
+		read_body(&m, sb, type);
+		toutf8(charset, dst, sbuf_buf(sb), sbuf_len(sb));
+		sbuf_free(sb);
 		if (m.depth)
 			read_bound(&m, dst);
 	}
@@ -282,4 +311,68 @@ int msg_demime(char *msg, long msglen, char **mod, long *modlen)
 	*modlen = sbuf_len(dst) - 1;
 	*mod = sbuf_done(dst);
 	return 0;
+}
+
+static char *cp1256[256] = {
+	[0x80] = "€", [0x81] = "پ", [0x82] = "‚", [0x83] = "ƒ",
+	[0x84] = "„", [0x85] = "…", [0x86] = "†", [0x87] = "‡",
+	[0x88] = "ˆ", [0x89] = "‰", [0x8A] = "ٹ", [0x8B] = "‹",
+	[0x8C] = "Œ", [0x8D] = "چ", [0x8E] = "ژ", [0x8F] = "ڈ",
+	[0x90] = "گ", [0x91] = "‘", [0x92] = "’", [0x93] = "“",
+	[0x94] = "”", [0x95] = "•", [0x96] = "–", [0x97] = "—",
+	[0x98] = "ک", [0x99] = "™", [0x9A] = "ڑ", [0x9B] = "›",
+	[0x9C] = "œ", [0x9D] = "‌", [0x9E] = "‍", [0x9F] = "ں",
+	[0xA1] = "،", [0xAA] = "ھ", [0xBA] = "؛", [0xBF] = "؟",
+	[0xC0] = "ہ", [0xC1] = "ء", [0xC2] = "آ", [0xC3] = "أ",
+	[0xC4] = "ؤ", [0xC5] = "إ", [0xC6] = "ئ", [0xC7] = "ا",
+	[0xC8] = "ب", [0xC9] = "ة", [0xCA] = "ت", [0xCB] = "ث",
+	[0xCC] = "ج", [0xCD] = "ح", [0xCE] = "خ", [0xCF] = "د",
+	[0xD0] = "ذ", [0xD1] = "ر", [0xD2] = "ز", [0xD3] = "س",
+	[0xD4] = "ش", [0xD5] = "ص", [0xD6] = "ض", [0xD7] = "×",
+	[0xD8] = "ط", [0xD9] = "ظ", [0xDA] = "ع", [0xDB] = "غ",
+	[0xDC] = "ـ", [0xDD] = "ف", [0xDE] = "ق", [0xDF] = "ك",
+	[0xE0] = "à", [0xE1] = "ل", [0xE2] = "â", [0xE3] = "م",
+	[0xE4] = "ن", [0xE5] = "ه", [0xE6] = "و", [0xE7] = "ç",
+	[0xE8] = "è", [0xE9] = "é", [0xEA] = "ê", [0xEB] = "ë",
+	[0xEC] = "ى", [0xED] = "ي", [0xEE] = "î", [0xEF] = "ï",
+	[0xF0] = "ً", [0xF1] = "ٌ", [0xF2] = "ٍ", [0xF3] = "َ",
+	[0xF4] = "ô", [0xF5] = "ُ", [0xF6] = "ِ", [0xF7] = "÷",
+	[0xF8] = "ّ", [0xF9] = "ù", [0xFA] = "ْ", [0xFB] = "û",
+	[0xFC] = "ü", [0xFD] = "‎", [0xFE] = "‏", [0xFF] = "ے",
+};
+
+static char *iso8859_6[256] = {
+	[0xAC] = "،", [0xBB] = "؛", [0xBF] = "؟", [0xC1] = "ء",
+	[0xC2] = "آ", [0xC3] = "أ", [0xC4] = "ؤ", [0xC5] = "إ",
+	[0xC6] = "ئ", [0xC7] = "ا", [0xC8] = "ب", [0xC9] = "ة",
+	[0xCA] = "ت", [0xCB] = "ث", [0xCC] = "ج", [0xCD] = "ح",
+	[0xCE] = "خ", [0xCF] = "د", [0xD0] = "ذ", [0xD1] = "ر",
+	[0xD2] = "ز", [0xD3] = "س", [0xD4] = "ش", [0xD5] = "ص",
+	[0xD6] = "ض", [0xD7] = "ط", [0xD8] = "ظ", [0xD9] = "ع",
+	[0xDA] = "غ", [0xE0] = "ـ", [0xE1] = "ف", [0xE2] = "ق",
+	[0xE3] = "ك", [0xE4] = "ل", [0xE5] = "م", [0xE6] = "ن",
+	[0xE7] = "ه", [0xE8] = "و", [0xE9] = "ى", [0xEA] = "ي",
+	[0xEB] = "ً", [0xEC] = "ٌ", [0xED] = "ٍ", [0xEE] = "َ",
+	[0xEF] = "ُ", [0xF0] = "ِ", [0xF1] = "ّ", [0xF2] = "ْ",
+};
+
+static void toutf8(char *charset, struct sbuf *dst, char *src, int len)
+{
+	int i;
+	char **map = NULL;
+	if (charset && !strcmp("windows-1256", charset))
+		map = cp1256;
+	if (charset && !strcmp("iso-8859-6", charset))
+		map = iso8859_6;
+	if (map) {
+		for (i = 0; i < len; i++) {
+			int c = (unsigned char) src[i];
+			if (map[c])
+				sbuf_str(dst, map[c]);
+			else
+				sbuf_chr(dst, c);
+		}
+	} else {
+		sbuf_mem(dst, src, len);
+	}
 }
